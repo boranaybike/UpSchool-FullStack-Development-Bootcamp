cbd32e91ad89eb1969fed6a2dc1938b31b18c7e8
ik3 <aybikeboran7140@gmail.com>
2023-03-07 19:04:59 +0300

    Add Ödev2

---
+++ Ödev2/UpSchool.Wasm/Common/Utilities/ConsoleLogger.cs
@@ -0,0 +1,13 @@
+﻿using UpSchool.Domain.Common;
+
+namespace UpSchool.Wasm.Common.Utilities
+{
+    public class ConsoleLogger:LoggerBase
+    {
+        public ConsoleLogger(string titanicFluteUrl) : base(titanicFluteUrl)
+        {
+
+        }
+
+    }
+}
---
+++ Ödev2/UpSchool.Wasm/Services/ToasterService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.JSInterop;
+using Shipwreck.BlazorJqueryToast;
+using UpSchool.Domain.Services;
+
+namespace UpSchool.Wasm.Services
+{
+    public class ToasterService:IToasterService
+    {
+        private readonly IJSRuntime _jsRuntime;
+        public ToasterService(IJSRuntime JsRuntime) {
+
+            _jsRuntime = JsRuntime;
+        }
+
+
+        public void ShowSuccess(string message) {
+            _jsRuntime.ShowToastAsync(new ToastOptions
+            {
+                Text = message,
+                Position = ToastPosition.TopCenter,
+                Heading = "UpSchool"
+            });
+        }
+
+    }
+}
